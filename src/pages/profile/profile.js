7ae9db63ddb5a43826b53aafeff24f75c021d102
VCITY-PC1\richard.navin <richard.dharmaraj@adabisc.com>
2019-05-20 15:30:29 +0300

    Init Anim

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -3,11 +3,18 @@ import Skills from './Skills';
 import React, { Component } from 'react'
 import profiledata from '../../data/profileData.json'
 import Education from './Education';
+import TweenLite from 'gsap';
 
 export default class profile extends Component {
   
   canRender=false;
   
+  constructor(props){
+    super(props);
+    this.myElement=null;
+    this.myTween=null;
+  }
+
   state = {
     profile:[]
   }
@@ -15,12 +22,16 @@ export default class profile extends Component {
   componentDidMount(){
     this.setState({profile:profiledata});
     this.canRender=true;
+    setTimeout(()=>{ this.playAnim()},50);
   }
-  
+  playAnim(){
+    this.myTween=TweenLite.to(this.myElement, .01, {width: "0%"});
+    this.myTween=TweenLite.to(this.myElement, 1, {width: "100%"});
+  }
   render() {
     if(this.canRender){
       return (
-        <div >
+        <div ref={div => this.myElement = div}>
         <Skills experiences={this.state.profile.experience} />
         <Education studies={this.state.profile.education}/>
         </div>
