b6d961b8350346f6ef19f8be1a0016fcfb7c307f
VCITY-PC1\richard.navin <richard.dharmaraj@adabisc.com>
2019-05-21 13:09:13 +0300

    Build file

---
+++ build/precache-manifest.6985b2f2df737ac39d6aeb95f34d7c88.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a327c0961f9e1652616896d91b105b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "810043f3afe6e1f5d667",
+    "url": "/static/css/2.f41ad63a.chunk.css"
+  },
+  {
+    "revision": "6c2d491426347715be9d",
+    "url": "/static/css/main.9be13782.chunk.css"
+  },
+  {
+    "revision": "810043f3afe6e1f5d667",
+    "url": "/static/js/2.2ca032d2.chunk.js"
+  },
+  {
+    "revision": "6c2d491426347715be9d",
+    "url": "/static/js/main.c6334f51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9af662a0a6ac9204774f182f9f3a9651",
+    "url": "/static/media/Inconsolata-Bold.9af662a0.ttf"
+  },
+  {
+    "revision": "2c22fca2e6627676b0cd60ec1be3beb2",
+    "url": "/static/media/Inconsolata-Regular.2c22fca2.ttf"
+  },
+  {
+    "revision": "484745d5640dde51f9ed85459fecb711",
+    "url": "/static/media/androids.484745d5.png"
+  },
+  {
+    "revision": "c0be4f518e9bba2eddd1c0fbfc72e575",
+    "url": "/static/media/arduino.c0be4f51.png"
+  },
+  {
+    "revision": "0c7a8e4c30e61a45116776896e605b83",
+    "url": "/static/media/basecamp.0c7a8e4c.png"
+  },
+  {
+    "revision": "98ed75bf661ed534c19aaa3d1b120062",
+    "url": "/static/media/csharp.98ed75bf.png"
+  },
+  {
+    "revision": "fbb32f1779b3444652d1109686dabf47",
+    "url": "/static/media/dare.fbb32f17.jpg"
+  },
+  {
+    "revision": "89dea3acb0aa78c9d77fed479778ebe5",
+    "url": "/static/media/git.89dea3ac.png"
+  },
+  {
+    "revision": "f14230b4c4c1b4a2a87191ea15ba1a57",
+    "url": "/static/media/header.f14230b4.jpg"
+  },
+  {
+    "revision": "ce98aed5b2bf2032de8294c4c8d574ed",
+    "url": "/static/media/linkedin.ce98aed5.png"
+  },
+  {
+    "revision": "35ad0284e7c80d56b3966aac85223437",
+    "url": "/static/media/pg.35ad0284.png"
+  },
+  {
+    "revision": "0f6558f88fb457e68f92c042df252892",
+    "url": "/static/media/reactjs.0f6558f8.png"
+  },
+  {
+    "revision": "b73a7a69ee5401dbc493a8a3a7042103",
+    "url": "/static/media/rich.b73a7a69.jpg"
+  },
+  {
+    "revision": "a80cbfff0e52d3aa4f0fc555ab18cd97",
+    "url": "/static/media/skill.a80cbfff.png"
+  },
+  {
+    "revision": "219b6e7ad7d79d394ed9cb85d05ffbd8",
+    "url": "/static/media/square.219b6e7a.jpg"
+  },
+  {
+    "revision": "c295b18579fb1d16b82e900246620d31",
+    "url": "/static/media/ug.c295b185.png"
+  },
+  {
+    "revision": "b233c4100ddbc343c9fd7550504d4b97",
+    "url": "/static/media/unity3d.b233c410.png"
+  },
+  {
+    "revision": "13e13db80468188ed7089df888a193b1",
+    "url": "/static/media/xcode.13e13db8.png"
+  }
+]);
